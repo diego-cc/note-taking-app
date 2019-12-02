0094d864e3562e5d0acf63524489cce072b736cc
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2019-12-02 19:59:50 +0800

    Updates

---
+++ precache-manifest.043053344b58b6a57304a3cd44e3a361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f96f7bcb410df193fe552947b4e191a",
+    "url": "/note-taking-app/index.html"
+  },
+  {
+    "revision": "8afef58e56355a2c35df",
+    "url": "/note-taking-app/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "c88f7f4f6560f4cc245e",
+    "url": "/note-taking-app/static/css/main.2fc1a809.chunk.css"
+  },
+  {
+    "revision": "8afef58e56355a2c35df",
+    "url": "/note-taking-app/static/js/2.e83f5a97.chunk.js"
+  },
+  {
+    "revision": "c88f7f4f6560f4cc245e",
+    "url": "/note-taking-app/static/js/main.3779f6e3.chunk.js"
+  },
+  {
+    "revision": "79165377dc96584c64a5",
+    "url": "/note-taking-app/static/js/runtime-main.17e60a2a.js"
+  }
+]);
